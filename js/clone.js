12ba684c80c1f8a645ffde8d8f67547ce6809ea5
CharleneSheep <65222348+CharleneSheep@users.noreply.github.com>
2020-08-10 16:43:28 +0800

    Update clone.js
    Charlene

--- js/clone.js
+++ js/clone.js
@@ -2,7 +2,19 @@
 // * 那么你实际是怎么做的？
 
 // * ---------------- 解决下文中缺少 clone 函数的问题……
-// 在这里写代码……
+function clone(obj){
+  let cloneObj = Array.isArray(obj) ? [] : {}
+  if(obj && typeof obj === "object"){
+    for(let i = 0; i < obj.length; i++){
+      if(obj[i] && typeof obj[i] === "object"){
+        cloneObj[i] = clone(obj[i])
+      }else{
+        cloneObj[i] = obj[i]
+      }
+    }
+  }
+  return cloneObj
+}
 
 // * ---------------- 实现的效果：
 
